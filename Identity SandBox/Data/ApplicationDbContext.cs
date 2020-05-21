0ab3c7cc897033648f8b36f132bf3fb9301cac33
Joshua Farin <farin.k.joshua@gmail.com>
2020-05-21 06:45:28 -1000

    Add project files.

---
+++ Identity SandBox/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Identity_SandBox.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        //Fresh slate
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
