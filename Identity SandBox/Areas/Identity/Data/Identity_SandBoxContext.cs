365db8b7c57c04a7d83827b7fa6cd0dd2a8c8782
Joshua Farin <farin.k.joshua@gmail.com>
2020-05-21 11:25:59 -1000

    Scaffold identity

---
+++ Identity SandBox/Areas/Identity/Data/Identity_SandBoxContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Identity_SandBox.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Identity_SandBox.Data
+{
+    public class Identity_SandBoxContext : IdentityDbContext<Identity_SandBoxUser>
+    {
+        public Identity_SandBoxContext(DbContextOptions<Identity_SandBoxContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ Identity SandBox/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Identity_SandBox.Areas.Identity.Data;
+using Identity_SandBox.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Identity_SandBox.Areas.Identity.IdentityHostingStartup))]
+namespace Identity_SandBox.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<Identity_SandBoxContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("Identity_SandBoxContextConnection")));
+
+                services.AddDefaultIdentity<Identity_SandBoxUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<Identity_SandBoxContext>();
+            });
+        }
+    }
+}
